990243b939e8ea0a188babd8707d696310c0333c
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-08-05 15:44:58 +0200

    js

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -1,23 +1,18 @@
-// mongo.connect(url, {useNewUrlParser: true}, (err, client) => {
-//   if (err) {
-//     console.error(err)
-//     return false
-//   }
-//   console.log('Conectado a Mongo DB...')
-//
-//   const db = client.db('mongo-demo')
-//   const collection = db.collection('tx')
-//
-//   collection.insertOne({name: 'Roger'}, (err, result) => {
-//     if (err) {
-//       console.error('Err InsertOne')
-//       console.error(err)
-//       return false
-//     }
-//     console.log('<<<<<<<<<<<< result >>>>>>>>>>>>')
-//     console.log(result.ops)
-//   })
-//
-// })
-//
-// module.exports = db
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+/**
+ * Transactions schema
+ */
+
+const TransactionsSchema = new Schema({
+  timestamps: true,
+  name: {
+    required: true,
+    type: String,
+    default: ''
+  }
+})
+
+mongoose.model('Transaction', TransactionsSchema)
+
